90dc9810c6979fe1165801da8c792444fb9eed01
Maksym Viter <safary.121@gmail.com>
2022-05-17 16:15:51 +0300

    fix namespace

--- wpforms-uploads-path-filter.php
+++ wpforms-uploads-path-filter.php
@@ -10,7 +10,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-use Innocode\WPFORMS_UPLOADS_PATH;
+use Innocode\Wpforms_Uploads_Path;
 
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
     require_once __DIR__ . '/vendor/autoload.php';
