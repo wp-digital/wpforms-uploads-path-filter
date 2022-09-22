a4ed06acf799f9181278fabb63e10b2d90929c77
MaksVeter <safary.121@gmail.com>
2022-09-22 16:29:40 +0300

    Create wpforms-uploads-path-filter.php

--- wpforms-uploads-path-filter.php
+++ wpforms-uploads-path-filter.php
@@ -10,7 +10,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-use Innocode\Wpforms_Uploads_Path;
+//use Innocode\Wpforms_Uploads_Path;
 
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
     require_once __DIR__ . '/vendor/autoload.php';
